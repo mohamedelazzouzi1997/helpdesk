b766838c5e14ee1500cd191ea7fb402564d69fd4
mohamed el azzouzi <med.elazzouzi.97@gmail.com>
2022-05-25 11:46:54 +0100

    date format

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -17,7 +17,7 @@
             @yield('page-style')
         @endif
         <!-- CSS only -->
-<link rel="stylesheet" href="{{asset('assets/plugins/dropify/css/dropify.min.css')}}"/>
+        <link rel="stylesheet" href="{{asset('assets/plugins/dropify/css/dropify.min.css')}}"/>
         <!-- Custom Css -->
         <link rel="stylesheet" href="{{asset('assets/css/style.min.css')}}">
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
